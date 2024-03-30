3e97ee9067882d7433bf1c8ae9345659776c1c56
ricky <rickykumarj@gmail.com>
2024-03-30 10:22:21 +0530

    new repo for string calculator

---
+++ spec/models/string_calculator_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe StringCalculator, type: :model do
+  describe ".add" do
+    it "returns 0 for an empty string" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it "returns the number for a single number string" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+  end
+end
