570ab5630d4ff0fb540bf67d3a969150f7b924c4
ricky <rickykumarj@gmail.com>
2024-03-30 11:08:24 +0530

    add no in string

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,3 +1,18 @@
 class StringCalculator
-  
+  def self.add(numbers)
+    delimiters = [',', "\n"]
+    
+    if numbers.start_with?("//")
+      custom_delimiter = numbers[2]
+      delimiters.push(custom_delimiter)
+      numbers = numbers.split("\n", 2)[1]
+    end
+
+    numbers_array = numbers.split(Regexp.union(delimiters)).map(&:to_i)
+    
+    negatives = numbers_array.select { |num| num < 0 }
+    
+
+    numbers_array.sum
+  end
 end
